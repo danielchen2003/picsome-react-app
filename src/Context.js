7bf78bb387daadbf645d3a52fd735e6ab42ded19
linfeng Chen <ku2003ku@gmail.com>
2022-08-03 09:20:08 -0400

    picsome

---
+++ src/Context.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+const Context = React.createContext()
+
+function ContextProvider({ children }) {
+  const [allPhotos, setAllPhotos] = useState([])
+
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAllPhotos(data))
+  }, [])
+
+  function toggleFavorite(id) {
+    const updatedArr = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        console.log(id)
+        console.log(!photo.isFavorite)
+        return { ...photo, isFavorite: !photo.isFavorite }
+      }
+      return photo
+    })
+    setAllPhotos(updatedArr)
+  }
+
+  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>
+}
+export { ContextProvider, Context }
---
+++ src/components/Image.js
@@ -0,0 +1,30 @@
+import React, { useState, useContext } from "react"
+import { Context } from "../Context"
+
+function Image({ className, img }) {
+  const [hovered, setHovered] = useState(false)
+  const { toggleFavorite } = useContext(Context)
+
+  const heartIcon = hovered && (
+    <i
+      className="ri-heart-line favorite"
+      onClick={() => toggleFavorite(img.id)}
+    ></i>
+  )
+
+  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
+  return (
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img src={img.url} className="image-grid" />
+      {heartIcon}
+      {cartIcon}
+    </div>
+  )
+}
+
+export default Image
